97c702a8b843adfea600b889223adc51da983123
chawuala <mrojas@econtainers.co>
2023-10-06 10:51:47 -0500

    Ajustes de colores

--- src/app/components/our-flavors/our-flavors.component.ts
+++ src/app/components/our-flavors/our-flavors.component.ts
@@ -11,33 +11,33 @@ export class OurFlavorsComponent {
     {
       titulo: 'Peto',
       texto: 'Un clásico suave y delicioso que te recordará a los sabores tradicionales de Colombia.',
-      image: 'https://comidaschilenas.com/wp-content/uploads/2022/02/Receta-de-arepas-de-maiz-venezolanas.jpg',
+      image: 'https://firebasestorage.googleapis.com/v0/b/arepas-el-eden.appspot.com/o/arepas-edit%2Fpeto-removebg.png?alt=media&token=25e5ca21-db44-4bae-92e7-e4c51adcb258&_gl=1*1nwozje*_ga*NTc1OTU2MDAwLjE2OTAxOTg2NDA.*_ga_CW55HF8NVT*MTY5NjYwNTE2NC4xMi4xLjE2OTY2MDU1MDguNDQuMC4w',
       buttonTxt: 'QUIERO ORDENAR',
-      url: 'https://api.whatsapp.com/send?phone=573177009946&text=Hola,%20estuve%20en%20su%20p%C3%A1gina%20y%20quisiera%20encargar%20arepas%20de%20peto',
+      url: 'https://api.whatsapp.com/send?phone=573108585105&text=Hola,%20estuve%20en%20su%20p%C3%A1gina%20y%20quisiera%20encargar%20arepas%20de%20peto',
       id: 'peto'
     },
     {
       titulo: 'Pelao',
       texto: 'Nuestra arepa pelao representa la simplicidad y la tradición culinaria colombiana.',
-      image: 'https://comidaschilenas.com/wp-content/uploads/2022/02/Receta-de-arepas-de-maiz-venezolanas.jpg',
+      image: 'https://firebasestorage.googleapis.com/v0/b/arepas-el-eden.appspot.com/o/arepas-edit%2Fpelao-removebg.png?alt=media&token=54b9ace1-a410-4d99-a51a-5f36fb77d054&_gl=1*110yi9u*_ga*NTc1OTU2MDAwLjE2OTAxOTg2NDA.*_ga_CW55HF8NVT*MTY5NjYwNTE2NC4xMi4xLjE2OTY2MDU0OTIuNjAuMC4w',
       buttonTxt: 'QUIERO ORDENAR',
-      url: 'https://api.whatsapp.com/send?phone=573177009946&text=Hola,%20estuve%20en%20su%20p%C3%A1gina%20y%20quisiera%20encargar%20arepas%20de%20maiz%20pelao',
+      url: 'https://api.whatsapp.com/send?phone=573108585105&text=Hola,%20estuve%20en%20su%20p%C3%A1gina%20y%20quisiera%20encargar%20arepas%20de%20maiz%20pelao',
       id: 'pelao'
     },
     {
       titulo: 'Sal',
       texto: 'La esencia misma de nuestras arepas colombianas, con su crujiente por fuera y suavidad por dentro.',
-      image: 'https://comidaschilenas.com/wp-content/uploads/2022/02/Receta-de-arepas-de-maiz-venezolanas.jpg',
+      image: 'https://firebasestorage.googleapis.com/v0/b/arepas-el-eden.appspot.com/o/arepas-edit%2FSal-removebg.png?alt=media&token=775a4718-2f64-4dc0-a526-7871318f5990&_gl=1*xq6t5o*_ga*NTc1OTU2MDAwLjE2OTAxOTg2NDA.*_ga_CW55HF8NVT*MTY5NjYwNTE2NC4xMi4xLjE2OTY2MDU0MjEuNjAuMC4w',
       buttonTxt: 'QUIERO ORDENAR',
-      url: 'https://api.whatsapp.com/send?phone=573177009946&text=Hola,%20estuve%20en%20su%20p%C3%A1gina%20y%20quisiera%20encargar%20arepas%20de%20sal',
+      url: 'https://api.whatsapp.com/send?phone=573108585105&text=Hola,%20estuve%20en%20su%20p%C3%A1gina%20y%20quisiera%20encargar%20arepas%20de%20sal',
       id : 'sal'
     },
     {
       titulo: 'Dulce',
       texto: 'Una opción perfecta para satisfacer tu gusto por lo dulce, ideal para cualquier momento del día.',
-      image: 'https://comidaschilenas.com/wp-content/uploads/2022/02/Receta-de-arepas-de-maiz-venezolanas.jpg',
+      image: 'https://firebasestorage.googleapis.com/v0/b/arepas-el-eden.appspot.com/o/arepas-edit%2Fdulce-removebg.png?alt=media&token=a99f1212-a2ee-4b1b-8571-ddd194412245&_gl=1*oi58dl*_ga*NTc1OTU2MDAwLjE2OTAxOTg2NDA.*_ga_CW55HF8NVT*MTY5NjYwNTE2NC4xMi4xLjE2OTY2MDU0NzUuNi4wLjA.',
       buttonTxt: 'QUIERO ORDENAR',
-      url: 'https://api.whatsapp.com/send?phone=573177009946&text=Hola,%20estuve%20en%20su%20p%C3%A1gina%20y%20quisiera%20encargar%20arepas%20de%20dulce',
+      url: 'https://api.whatsapp.com/send?phone=573108585105&text=Hola,%20estuve%20en%20su%20p%C3%A1gina%20y%20quisiera%20encargar%20arepas%20de%20dulce',
       id: 'dulce'
 
     }
